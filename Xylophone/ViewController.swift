bb457d687f5551c6776b0c7f04b2c9daaeaf8cba
KushagraSinha2002 <kushagrasinha2002@gmail.com>
2022-06-15 17:00:28 +0530

    -Added Button sound -Completed Xylophone App

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import SwiftUI
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.systemGray6
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(sondName: sender.currentTitle!)
+        
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(sondName: String) {
+        let url = Bundle.main.url(forResource: sondName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
